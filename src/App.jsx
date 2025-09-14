9e3732243b13df656a0a7914b7a22a4d31e5ac28
Won Lee <wonseob2207@gmail.com>
2025-09-13 18:01:04 -0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Iridescence from './Iridescence';
 
 export default function App() {
   return (
-    <div>
-    </div>
+    <Iridescence
+        color={[1, 1, 1]}
+        mouseReact={false}
+        amplitude={0.1}
+        speed={1.0}
+        />
   );
 }
+
