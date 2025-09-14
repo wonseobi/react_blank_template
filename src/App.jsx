23bc9f5d1c918cf8bd73eef95c7f8fe465d07d73
Won Lee <wonseob2207@gmail.com>
2025-09-13 18:04:13 -0600

    new template

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import Iridescence from './Iridescence';
 
 export default function App() {
   return (
-    <Iridescence
+    <div style={{ position: 'relative', width: '100%', height: '600px' }}>
+      <Iridescence
         color={[1, 1, 1]}
         mouseReact={false}
         amplitude={0.1}
         speed={1.0}
-        />
+        style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+      />
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          color: 'white',
+          fontSize: '48px',
+          fontWeight: 'bold',
+          pointerEvents: 'none', // so it doesn’t block mouse events
+        }}
+      >
+        Hello World
+      </div>
+    </div>
   );
 }
 
